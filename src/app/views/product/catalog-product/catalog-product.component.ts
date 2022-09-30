d302b1ba1186c7cb4e46e01683498d56cfc33a7a
Jehmes Thales <jeihmesthales@hotmail.com>
2022-09-30 18:49:38 -0300

    ATIVIDADE-004 Ordenar os itens

--- src/app/views/product/catalog-product/catalog-product.component.ts
+++ src/app/views/product/catalog-product/catalog-product.component.ts
@@ -9,6 +9,8 @@ import { ServiceProductService } from 'src/app/services/service-product.service'
 })
 export class CatalogProductComponent implements OnInit {
 
+  chosenFilterColumn = "";
+  chosenFilterType = "";
   allproducts: Product[] = [];
 
   constructor(private service: ServiceProductService) { }
@@ -22,6 +24,77 @@ export class CatalogProductComponent implements OnInit {
       this.allproducts = data;
       console.log(data)
     })
+  }
+
+  filterChange() {
+    switch (this.chosenFilterColumn) {
+      case "quantity":
+        if (this.chosenFilterType === "ASC")
+        this.allproducts.sort(this.compareQuantityAsc);
+        else
+        this.allproducts.sort(this.compareQuantityDesc);
+        break;
+      case "defective":
+        if (this.chosenFilterType === "ASC")
+        this.allproducts.sort(this.compareDefectiveAsc);
+        else
+        this.allproducts.sort(this.compareDefectiveDesc);
+        break;
+      case "name":
+        if (this.chosenFilterType === "DESC")
+        this.allproducts.sort(this.compareNameDesc);
+        else
+        this.allproducts.sort(this.compareNameAsc);
+        break;
+    }
+  }
+
+  compareNameAsc(a: any, b: any) {
+    if (a.name < b.name)
+      return -1;
+    if (a.name > b.name)
+      return 1;
+    return 0;
+  }
+
+  compareNameDesc(a: any, b: any) {
+    if (a.name > b.name)
+      return -1;
+    if (a.name < b.name)
+      return 1;
+    return 0;
+  }
+
+  compareQuantityAsc(a: any, b: any) {
+    if (a.quantity < b.quantity)
+      return -1;
+    if (a.quantity > b.quantity)
+      return 1;
+    return 0;
+  }
+
+  compareQuantityDesc(a: any, b: any) {
+    if (a.quantity > b.quantity)
+      return -1;
+    if (a.quantity < b.quantity)
+      return 1;
+    return 0;
+  }
+
+  compareDefectiveAsc(a: any, b: any) {
+    if (a.defective < b.defective)
+      return -1;
+    if (a.defective > b.defective)
+      return 1;
+    return 0;
+  }
+
+  compareDefectiveDesc(a: any, b: any) {
+    if (a.defective > b.defective)
+      return -1;
+    if (a.defective < b.defective)
+      return 1;
+    return 0;
   }
 
 }
